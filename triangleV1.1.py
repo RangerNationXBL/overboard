ecae16bf99a347e1875060eb2d2863e5c4b89797
Chris <brown.christopher1978@gmail.com>
2023-08-31 00:04:03 -0700

    new version

---
+++ triangleV1.1.py
@@ -0,0 +1,26 @@
+# Triangle version 1.1
+# Author: Chris
+# Date August 2023
+
+import tkinter as tk
+from tkinter import ttk
+import math
+
+# Configure the window here. 
+window = tk.Tk()
+window.geometry('800x600')
+window.title('Calculate a Triangle in many ways.')
+window.configure( bg='gray')
+
+# Grif styling
+
+# Widgets
+canvas = tk.Canvas(width=800, height=400)
+canvas.grid()
+frame_1 = ttk.Frame(width=800, height=200)
+frame_1.grid()
+
+# Grid configuration
+
+
+window.mainloop()
